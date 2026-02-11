a1d5320aa39a0bb9c377c3d5091c5b39a962cc9b
bigman79 <64438880+bigman79@users.noreply.github.com>
2026-02-11 02:45:55 -0800

    Added Stress testing to main
    Probably will not keep around this code its pretty shitty.

--- marketscrape/stock_history.py
+++ marketscrape/stock_history.py
@@ -1,48 +1,28 @@
 import alpaca.data.historical as alpaca_historical
-from alpaca.data.models.bars import BarSet, Bar
 from alpaca.data.requests import StockBarsRequest
 from alpaca.data.timeframe import TimeFrame
 from datetime import datetime
 import pandas as pd
 
-class stock_history():
-    """class for retrieving and storing historical stock data for a given symbol."""
+class StockHistoryBatch:
+    """Class for retrieving historical stock data for multiple symbols in one batch."""
     
-    def __init__(self, symbol: str, historical_client: alpaca_historical.StockHistoricalDataClient):
-        self._symbol = symbol
+    def __init__(self, historical_client: alpaca_historical.StockHistoricalDataClient):
         self._historical_client = historical_client
-        self._bars: pd.DataFrame = pd.DataFrame()
-        
 
-    def get_historical_bars(self, start: datetime, end: datetime) -> pd.DataFrame:
-        """Retrieve historical bars for the symbol between start and end datetimes."""
+    def get_historical_bars(self, symbols: list, start: datetime, end: datetime, timeframe=TimeFrame.Day) -> pd.DataFrame:
+        """
+        Retrieve historical bars for multiple symbols in a single batch request.
+        Returns a MultiIndex DataFrame (symbol, timestamp).
+        """
+        request = StockBarsRequest(
+            symbol_or_symbols=symbols,
+            start=start,
+            end=end,
+            timeframe=timeframe
+        )
         
-        request = StockBarsRequest(symbol_or_symbols=self._symbol, start=start, end=end, timeframe=TimeFrame.Minute)
-        barset: BarSet = self._historical_client.get_stock_bars(request)
-        bars_list = []
-
-        for bar in barset[self._symbol]:
-            bar: Bar
-            bars_list.append({
-                "timestamp": bar.timestamp,
-                "open": bar.open,
-                "high": bar.high,
-                "low": bar.low,
-                "close": bar.close,
-                "volume": bar.volume
-            })
-
-        self._bars = pd.DataFrame(bars_list)
-        self._bars.set_index("timestamp", inplace=True)
-        return self._bars
-    
+        # The .df property automatically converts the response to a clean Pandas DataFrame
+        barset_df = self._historical_client.get_stock_bars(request).df
         
-        
-        
-       
-        
-
-        
-
-    
-        
+        return barset_df
